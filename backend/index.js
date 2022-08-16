76164e92a8057faa2762d1cf93380d8e6213e520
andreachirico11 <andreachirico11@gmail.com>
2022-08-16 23:23:31 +0200

    BE: pdf generated

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,25 @@ const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cheerio = require('cheerio');
+const pdf = require('html-pdf');
+const fs = require('fs');
+
 const { PORT, FILE_URL, GITHUB_TOKEN } = process.env;
+const TEMPORARY_PDF_NAME = 'temp.pdf';
+const fileOptions = {
+  format: 'A4',
+  orientation: 'portrait',
+  border: {
+    top: '0',
+    bottom: '0',
+  },
+};
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/test', async (req, res) => {
+app.get('/cv', async (req, res) => {
   try {
     const resp = await axios.get(FILE_URL, {
       headers: {
@@ -17,7 +30,23 @@ app.get('/test', async (req, res) => {
         authorization: GITHUB_TOKEN,
       },
     });
-    res.send(resp.data);
+    const $ = cheerio.load(resp.data);
+    const htmlContent = $('pre').html();
+    const pdfFile = await new Promise((res, rej) => {
+      pdf.create(htmlContent, fileOptions).toFile(TEMPORARY_PDF_NAME, function (err, file) {
+        if (err) {
+          rej('error during pdf creation');
+        }
+        res(file);
+      });
+    });
+    res.setHeader('Content-Type', 'application/pdf');
+    res.sendFile(pdfFile.filename, function (err) {
+      if (err) {
+        console.log(err);
+      }
+      fs.unlinkSync(TEMPORARY_PDF_NAME);
+    });
   } catch (e) {
     console.error(e);
     if (e.response && e.response.data && e.response.data.message) {
@@ -28,7 +57,7 @@ app.get('/test', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Github NodeJS API app!');
+  res.status(500).json('Not Found');
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
