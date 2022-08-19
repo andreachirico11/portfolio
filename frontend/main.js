9260421f7b6f3ca25153c9f9f70c33f43cbcebc6
andreachirico11 <andreachirico11@gmail.com>
2022-08-19 21:21:38 +0200

    FE: added loading and dialogs to CV request

--- frontend/main.js
+++ frontend/main.js
@@ -2,25 +2,26 @@ const cvUrl = (prodUrl || devUrl) + '/cv';
 const NO_TOKEN = 'No token';
 
 document.getElementById('token-submit').addEventListener('click', async function () {
+  startLoadingBar();
   try {
     const input = document.getElementById('token');
     if (!input.value) {
-      alert('missing token');
-      throw new Error('missing token');
+      openModal('The input is empty', 'You must provide a value!');
+      return;
     }
-    const file = await fetchFile(input.value);
-    downloadFile(file);
+    downloadFile(await fetchFile(input.value));
   } catch (e) {
     if (e.message === NO_TOKEN) {
-      alert('Missing or wrong token');
+      openModal('Something went wrong...', 'Your passcode is incorrect');
+    } else {
+      openModal('Something went wrong...', 'There was an error generating the resume');
     }
-    console.error(e);
+  } finally {
+    stopLoadingBar();
   }
 });
 
-document.getElementById('modal-button').addEventListener('click', toggleModal);
-
-document.getElementById('email-submit').addEventListener('click', toggleModal);
+document.getElementById('modal-button').addEventListener('click', closeModal);
 
 function fetchFile(token) {
   return fetch(cvUrl, {
@@ -37,31 +38,38 @@ function fetchFile(token) {
 }
 
 function downloadFile(blob) {
+  const downloader = document.getElementById('downloadAnchor');
   let objUrl;
   try {
-    const downloader = document.getElementById('downloadAnchor');
     objUrl = URL.createObjectURL(blob);
     downloader.href = objUrl;
     downloader.download = 'Resume Andrea Chirico';
     downloader.click();
+  } catch (error) {
+    openModal('Something went wrong...', 'The received file is corrupted');
+  } finally {
     downloader.href = null;
     downloader.download = null;
-    URL.revokeObjectURL(objUrl);
-  } catch (error) {
-    throw new Error('error downloading');
-  } finally {
     URL.revokeObjectURL(objUrl);
   }
 }
 
-function toggleModal() {
-  document.body.classList.toggle('show-modal');
+function openModal(title, message) {
+  document.getElementById('modal-title').textContent = title;
+  document.getElementById('modal-message').textContent = message;
+  document.body.classList.add('show-modal');
+}
+
+function closeModal() {
+  document.body.classList.remove('show-modal');
+  document.getElementById('modal-title').textContent = '';
+  document.getElementById('modal-message').textContent = '';
 }
 
 function startLoadingBar() {
-  document.getElementById('loading').style.display = 'visible';
+  document.body.classList.add('show-loading');
 }
 
 function stopLoadingBar() {
-  document.getElementById('loading').style.display = 'none';
+  document.body.classList.remove('show-loading');
 }
