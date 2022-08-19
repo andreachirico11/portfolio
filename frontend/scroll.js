9c4e2b6723998ec62b75a991be58eeead5a923bc
andreachirico11 <andreachirico11@gmail.com>
2022-08-19 15:18:24 +0200

    FE: animations

--- frontend/scroll.js
+++ frontend/scroll.js
@@ -1,14 +1,17 @@
 const heightToShowElement = 100;
+const heightToChangeActiveLink = 50;
 
 window.addEventListener('scroll', () => {
   reveal('down');
   reveal('left');
   reveal('right');
+  checkVisibleSection();
 });
 
 reveal('down');
 reveal('left');
 reveal('right');
+checkVisibleSection();
 
 function reveal(className) {
   const elementsToShow = document.querySelectorAll('.' + className);
@@ -20,4 +23,24 @@ function reveal(className) {
       elementsToShow[i].classList.remove('show');
     }
   }
+}
+
+function checkVisibleSection() {
+  const sections = document.querySelectorAll('section');
+  Array.from(sections).forEach((section) => {
+    if (scrollY >= section.offsetTop - heightToChangeActiveLink) {
+      activateLinkBySectionId(section.id);
+    }
+  });
+}
+
+function activateLinkBySectionId(idToActivate) {
+  const links = document.getElementsByTagName('nav')[0].children;
+  Array.from(links).forEach((link) => {
+    if (link.getAttribute('href') === '#' + idToActivate) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
 }
