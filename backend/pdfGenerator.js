9ae67df174886645e97a8776c07b912c06813e1b
andreachirico11 <andreachirico11@gmail.com>
2022-08-19 08:06:47 +0200

    BE: build fixes

--- backend/pdfGenerator.js
+++ backend/pdfGenerator.js
@@ -2,7 +2,7 @@ const { PORT } = require('./environments');
 const puppeteer = require('puppeteer');
 
 module.exports = async function () {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto(`http://localhost:${PORT}/temp-html`);
   const result = await page.pdf({ format: 'A4' });
