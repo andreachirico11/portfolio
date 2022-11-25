d674fc7bea55b425986fecf5875cc29046bdc127
andreachirico11 <andreachirico11@gmail.com>
2022-11-25 10:22:50 +0100

    chore: updated envs

--- environments.ts
+++ environments.ts
@@ -11,4 +11,22 @@ export default class Environments {
   static get CV_TITLE() {
     return process.env.NEXT_PUBLIC_CV_TITLE || '';
   }
+  static get FILE_URL() {
+    return process.env.FILE_URL || '';
+  }
+  static get GITHUB_TOKEN() {
+    return process.env.GITHUB_TOKEN || '';
+  }
+  static get TOKEN() {
+    return process.env.TOKEN || '';
+  }
+  static get SENDGRID_API_KEYPERSONAL_MAIL() {
+    return process.env.SENDGRID_API_KEYPERSONAL_MAIL || '';
+  }
+  static get PERSONAL_MAIL() {
+    return process.env.PERSONAL_MAIL || '';
+  }
+  static get PERSONAL_TRANSPORT_MAIL() {
+    return process.env.PERSONAL_TRANSPORT_MAIL || '';
+  }
 }
