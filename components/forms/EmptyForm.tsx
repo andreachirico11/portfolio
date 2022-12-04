185650ba065a5d60642668b070a95000c2b3b365
andreachirico11 <andreachirico11@gmail.com>
2022-12-04 22:55:36 +0100

    fix: blocked form event

--- components/forms/EmptyForm.tsx
+++ components/forms/EmptyForm.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, FormEvent } from 'react';
 import { useShowAnimationContext } from '../../context/ShowAnimationContext';
 import { AnimationType } from '../../types';
 
@@ -8,9 +8,13 @@ interface Props {
 
 export const EmptyForm: FC<Props> = ({ onSubmit }) => {
   const animationRef = useShowAnimationContext<HTMLDivElement>(AnimationType.right);
+  const submit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit();
+  };
   return (
     <div className='w-3/4 max-w-xl lg:w-96' ref={animationRef}>
-      <form className='w-100' onSubmit={onSubmit}>
+      <form className='w-100' onSubmit={submit}>
         <h4 className='text-xl text-center mb-7 font-code text-custom-yellow sm:text-3xl md:text-4xl'>
           Ask for my Resume!
         </h4>
